c8d0a250ef64dbaaa68e0b1ce3f2dd8fd30417bc
Amresh Kumar <akumarcpr021@gmail.com>
2024-04-03 23:26:12 +0530

    Add Nav Bar successfully

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 // import Signup from "./Pages/Signup/Signup";
 import Login from "./Pages/Login/Login";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
       {/* <Signup /> */}
+      <Navbar />
     </>
   );
 }
