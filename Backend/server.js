a1c0909b4e486b2e6fe060e0bec70bf29f34ef97
arpitt45 <arpitkashyap903@gmail.com>
2024-04-07 19:42:22 +0530

    Completed Backend , Created user and message routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authroutes.js"
+import messageRoutes from "./routes/messageroutes.js"
+import userRoutes from "./routes/userroutes.js"
 import connectToMongodb from "./DB/connectToMongodb.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,7 +12,11 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());  //parse incoming request with json payloads
+app.use(cookieParser());
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
+
 
 // app.get("/", (req, res)=>{
 //    res.send("hello world");
