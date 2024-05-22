67eab14139f1c5c6d61fcbc7e0d174dcb69dd763
arpitt45 <arpitkashyap903@gmail.com>
2024-05-22 22:24:19 +0530

    Add vite+react

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,12 +5,8 @@ import { Route, Routes } from "react-router-dom";
 
 import Signup from "./Pages/Signup/Signup";
 import Login from "./Pages/Login/Login";
-<<<<<<< HEAD
-import Home from "./Pages/Home/Home";
-=======
 import {Toaster} from 'react-hot-toast';
 // import Home from "./Pages/Home/Home";
->>>>>>> 3a3be693e83c68373af6ffdade56b00de656b483
 // import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
