388975e13a292226d8b0c35c04e0e2d0d8b66740
deathwish07 <ayanaratharva@gmail.com>
2024-05-24 17:07:04 +0530

    Get message functionality

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,13 +1,14 @@
 import { createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
-export const userAuthContext = () => {
-    return useContext(AuthContext);
-}
 
-export const AuthContextProvider = ({children}) => {
-    const [authUser, setAuthUser] = useState(JSON.parse((localStorage.getItem("chat-user") || null)))
-    return <AuthContext.Provider value={{authUser,setAuthUser}}>
-        {children}
-    </AuthContext.Provider>
-}
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuthContext = () => {
+	return useContext(AuthContext);
+};
+
+export const AuthContextProvider = ({ children }) => {
+	const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem("chat-user")) || null);
+
+	return <AuthContext.Provider value={{ authUser, setAuthUser }}>{children}</AuthContext.Provider>;
+};
